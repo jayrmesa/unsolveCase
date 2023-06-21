30360966420139d30ee3df70a63170ebb169e3bf
Arnold Mesa Jr <jayrmesa@gmail.com>
2023-06-21 14:04:57 -0600

    added js components with basic structure

---
+++ src/components/case.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+const Case = () => {
+  const [currentCase, setCurrentCase] = useState(null);
+  const [discoveredClues, setDiscoveredClues] = useState([]);
+  const [identifiedSuspects, setIdentifiedSuspects] = useState([]);
+  const [isSolved, setIsSolved] = useState(false);
+
+  // Add more state variables 
+
+  // Code the logic and rendering for the Game component
+
+  return (
+    // JSX code for rendering the Game component
+    <div>
+      {/* Render the case, clues, suspects, and other game elements */}
+    </div>
+  );
+};
+
+export default Case;
